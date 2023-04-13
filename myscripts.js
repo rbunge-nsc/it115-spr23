050a71410509bdad324bd1e51bc455267e2a9c07
jjemi8884 <122662713+jjemi8884@users.noreply.github.com>
2023-04-12 19:01:47 -0700

    Add files via upload
    Added changes to scripts

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+
+function showAlt(x) {
+  document.getElementById("alttext_modified by Justin Jemison").innerHTML=x.alt;
+}
+
+function hideAlt(x) {
+  document.getElementById("alttext_modified by Justin Jemison").innerHTML="";
+}
